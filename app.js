eb217f92a88dcad38cadad4d8ef382d3085f7676
fanxh <fanxh@calis.edu.cn>
2018-06-29 15:31:13 +0800

    #1 add files

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const route = require('koa-route');
 var index = require('./routes/index');
 const app = new Koa();
 const fs = require('fs.promised');
+const serve = require('koa-static');
+const path = require('path');
 
-const logger = (ctx, next) => {
+const logger = async(ctx, next) => {
     console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
-    next()
+    await next();
 }
 
 const other = async function (ctx, next) {
     ctx.response.type = 'html';
     let r=await fs.readFile('./views/index.html', 'utf8');
     ctx.response.body =r;
-    next()
+   
 };
+const static = serve(path.join(__dirname,'views')); //静态资源处理  views是静态资源文件夹
 app.use(logger);
-app.use(other);
+app.use(static);
+//app.use(other);
 
 app.listen(3001);
